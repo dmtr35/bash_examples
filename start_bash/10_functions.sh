657fc503b3f3f396432bd797a2f993b530de138f
dmtr35 <nuke-id@yandex.ru>
2025-12-16 18:49:00 +0200

    Auto-commit: 16-12-2025 18:49:00 Added:    start_bash/20_arrey.sh Modified: .vscode/launch.json, start_bash/10_functions.sh

--- start_bash/10_functions.sh
+++ start_bash/10_functions.sh
@@ -1,4 +1,41 @@
 #!/bin/bash
+# example:
+# say_name(){
+#     echo "Black Hat Bash"
+# }
+
+
+# Эта функция проверяет, равен ли текущий идентификатор пользователя нулю.
+check_if_root(){
+    if [ "${EUID}" -eq "0" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+if check_if_root; then
+    echo "User is root"
+else
+    echo "User is not root"
+fi
+# ===========================================================
+
+test="here"
+echo "zero_arg: ${0}"
+echo "first: ${1}, second: ${2}, third: ${3}"
+print_args(){
+    echo "${test}"
+    echo "zero_arg: ${0}"
+    echo "first: ${1}, second: ${2}, third: ${3}"
+}
+
+print_args
+print_args No Starch Press
+# ===========================================================
 
 
 
+
+
+exit 0
